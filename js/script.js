974b37350e20e69c3dfaad854f487ec1bc35561c
Stas <ragioniere1980@gmail.com>
2022-03-21 21:30:05 +0300

    first commt

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// alert('Буду проституткой!');
+
+
+// const result = confirm('Будешь проституткой?');
+
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18 лет?", "18");
+
+// console.log(answer + 5);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Stas";
+
+// alert(`Привет, ${user}`);
+
+// answers[0] = prompt('Имя?', '');
+// answers[1] = prompt('Возраст?', '');
+// answers[2] = prompt('Кря?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+console.log('5' + " - object");
+console.log(5 + "4");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(incr++);
+console.log(incr);
+// console.log(decr--);
+
+let a = 8;
+
+console.log(typeof(a));
+
+console.log(2 + 2 * 2 !== 8);
+
+const isChecked = false,
+      isClose = false;
+      
+console.log(isChecked || !isClose);
+
+
+
