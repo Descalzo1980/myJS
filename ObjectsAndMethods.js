c31caeeffca7de481a92c94263becee08d533400
Stas <ragioniere1980@gmail.com>
2022-04-02 08:01:59 +0300

    aboutObjecksAndMethods

--- ObjectsAndMethods.js
+++ ObjectsAndMethods.js
@@ -12,21 +12,44 @@ const options = {
     }
 };
 
+console.log(Object.keys(options));// массив получаем на выходе
+
 // console.log(options["colors"]["border"]);
 
 // delete options.name;//удаление свойства
 
-// console.log(options);
+// // console.log(options);
 
-for (let key in options){//перебор ключ-значение
-     if (typeof(options[key]) === 'object'){
-         for (let i in options[key]){ //перебор объекта внутри объекта
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);  
-         }
-     }else{
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-     }
-}
+// let counter = 0;/*подсчет кол-ва ключ-пара. 
+//                     по факту сколько раз прошел цикл по парам.*/
 
+// // for (let key in options){//перебор ключ-значение
+// //      if (typeof(options[key]) === 'object'){
+// //          for (let i in options[key]){ //перебор объекта внутри объекта
+// //             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// //             /*counter++;//если убрать здесь по посчитаем 
+// //             без вложенности. получим 3 */
+// //          }
+// //      }else{
+// //         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// //         counter++;
+// //      }
+// // }
 
+// console.log(counter);
+
+// for (let key in options){//перебор ключ-значение
+//     // if (typeof(options[key]) === 'object'){
+//     //     for (let i in options[key]){ //перебор объекта внутри объекта
+//     //        console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//     //        /*counter++;//если убрать здесь по посчитаем 
+//     //        без вложенности. получим 3 */
+//     //     }
+//     // }else{
+//     //    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//        counter++;
+//     // }
+// }
+
+// console.log(counter); //4 простой перебор
 
